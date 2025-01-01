b9ef9afdfbeb141281d3d9ff6bb124b9522418e5
Girish Barhate <girish.barhate@gmail.com>
2025-01-01 21:30:00 +0530

    create playground app for phidata

--- Build_AgenticAI_FinancialAgent/playground.py
+++ Build_AgenticAI_FinancialAgent/playground.py
@@ -4,3 +4,53 @@ import phi.api
 from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo #Enables Agent to search the web for information
+
+import os
+import pathlib
+from phi.playground import Playground,serve_playground_app
+
+#Import API Keys for PHIDATA and GROQ
+#if using google colab
+from google.colab import userdata
+GROQ_API_KEY=userdata.get('GROQ_API_KEY')
+PHI_API_KEY=userdata.get('PHI_API_KEY')
+
+#os.environ["GROQ_API_KEY"] = GROQ_API_KEY # Setting the Groq API key as an environment variable
+#os.environ["PHI_API_KEY"] = PHI_API_KEY # Setting the Groq API key as an environment variable
+
+phi.api = GROQ_API_KEY
+#os.environ["GROQ_API_KEY"] = GROQ_API_KEY # Setting the Groq API key as an environment variable
+os.environ["PHI_API_KEY"] = PHI_API_KEY # Setting the Groq API key as an environment variable
+
+## Web search Agent 
+web_search_agent = Agent(
+    name ="Web Search Agent",
+    role = "Search the web for the information",
+    model = Groq(id='llama3-groq-70b-8192-tool-use-preview'),
+    tools = [DuckDuckGo()],
+    instructions = ['Always include sources'],
+    show_tool_calls = True,
+    markdown = True
+)
+
+## Financial Agent
+finance_agent = Agent(
+    name = "Finance AI Agent",
+    model =Groq(id='llama3-groq-70b-8192-tool-use-preview'),
+    tools = [
+        YFinanceTools( stock_price=True,analyst_recommendations=True, stock_fundamentals = True,company_news = True)
+        ],
+    instructions = ['Use Tables to display the datasources'],
+    show_tool_calls = True,
+    markdown = True
+)
+
+
+
+## Create App
+
+app = Playground(agents = [finance_agent,web_search_agent]).get_app()
+
+if __name__ == "__main__":
+    serve_playground_app("playground:app",reload=True)
+
